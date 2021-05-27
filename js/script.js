910185e0eab52a3cb54dca4552589b2d69a005e9
Tesfaye Mekonnen <tesfayir@yahoo.com>
2021-05-27 00:04:42 -0500

    All files are ready

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
 
-$(document).ready(function () {
-    $("#beakal").hide(300).show(1000).hide(300).show(1000);
-
-    $("#beakal").css({
-      color: "white",
-    });
-
-    alert($("#beakal").text());
-    console.log($("#beakal").text());
-  });
